ebd1cbfbd3c5de72e914f6935ffa913ca8db8b63
csn <shaonan.cheng@5imakeup.com>
2021-08-03 11:02:33 +0800

    lnmp

---
+++ www/myapp/blog/app/Http/Controllers/User/UserController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Unirest\Response;
+use App\Models\User;
+
+use Illuminate\Support\Facades\DB;
+
+
+class UserController extends Controller
+{
+
+    public function index()
+    {
+
+
+
+
+       /*$res=[
+          "aa" =>1,
+          "bb" =>2
+        ];
+
+        return Response()->json($res); */
+
+        $input=Request()->input();
+
+       // $userdata=DB::select(" select * from user limit 1 ");
+        $names=[];
+        $i=1;
+        DB::table('campaign')->orderBy('ID')->chunk(5,function($users) use(&$names,$i){
+
+            foreach ($users as $user){
+                $names[]=$user->Title;
+                echo "<pre/>第".$i."ccc";
+                echo $user->Title;
+                echo "</pre>";
+                $i++;
+            }
+
+
+        });
+
+        //var_dump($names);
+    }
+
+
+    public function jugg(){
+
+
+        /*$res=DB::table('User')->where('uid',1)->orwhere(function($query){
+
+            $query->whereDate("CreateTime",'<',"2016-01-01")->whereTime("CreateTime","15:00");
+
+        })->get();
+
+        var_dump($res); */
+
+
+        $user=User::FindorFail(2183);
+
+        return $user;
+
+
+    }
+
+    public function testinput(){
+
+
+        //return request()->input();
+        return view('welcome');
+
+    }
+
+}
---
+++ www/myapp/blog/routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get("/foor",function(){
+
+   return "hello world";
+
+});
+
+Route::get("/user","User\UserController@index");
+Route::get("/jugg","User\UserController@jugg");
+
+Route::match(["get","Post"],"res","User\UserController@index");
+
+Route::get('/routename',function(){
+
+    return "name";
+
+})->name('rname');
+
+Route::prefix('admin')->group(function(){
+
+   Route::get("user",function(){
+
+      return 1;
+   });
+
+   Route::get("dash",function(){
+
+      return 2;
+
+   });
+
+});
+
+Route::get("getpara/{id?}",function($id=1){
+
+    return $id;
+
+});
+
+Route::get('testinput','User\UserController@testinput');
